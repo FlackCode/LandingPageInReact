a4189631059fe1f229592a18f53552c404d60371
flack <163582560+FlackCode@users.noreply.github.com>
2024-04-09 18:44:07 +0200

    Finished Information Section

--- Landing Page/src/App.jsx
+++ Landing Page/src/App.jsx
@@ -1,10 +1,13 @@
 import Header from "./components/Header";
+import Information from "./components/Information";
 
 
 function App() {
   return (
+    <>
       <Header />
-      
+      <Information />
+    </>
   )
 }
 export default App;
