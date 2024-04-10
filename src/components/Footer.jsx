48941848dc26687c9d591ceb521111b66995b477
flack <163582560+FlackCode@users.noreply.github.com>
2024-04-10 18:44:48 +0200

    Delete src directory

--- src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-export default function Footer() {
-    return (
-        <>
-            <div className="xl:px-48 xl:py-8 md:px-24 md:py-8 xsm:px-12 xsm:py-2 xl:text-base md:text-base xsm:text-xs bg-gray-800 text-white flex justify-center">
-                <p>Copyright © The Odin Project 2024</p>
-            </div>
-        </>
-    )
-}
